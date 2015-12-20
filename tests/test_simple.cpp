5064922629e03b3bc9c71f8ee59717c33a3f190a
Mathieu Tarral <mathieu.tarral@gmail.com>
2015-12-20 12:44:56 +0100

    use getpid for test_simple

--- tests/test_simple.cpp
+++ tests/test_simple.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <sysinfo.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <boost/algorithm/string/join.hpp>
 
 int main(int argc, char *argv[])
@@ -7,10 +9,12 @@ int main(int argc, char *argv[])
     argc = argc;
     argv = argv;
 
+    // get current pid
+    pid_t pid = getpid();
     // get first process owned by the user
-    struct process_info_t pinfo = getProcessDetail(1);
+    struct process_info_t pinfo = getProcessDetail(pid);
 
-    std::cout << "details for PID : 1" << std::endl;
+    std::cout << "details for PID : " << pid << std::endl;
     std::cout << "fd : " << pinfo.fds.size() << std::endl;
     for (auto const &pair : pinfo.fds)
     {
