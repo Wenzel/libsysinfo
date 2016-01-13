b3a7952b276faeab9454f856b8144d89b88b2d54
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-01-13 01:23:23 +0100

    fix compilation

--- src/processinfo.cpp
+++ src/processinfo.cpp
@@ -49,7 +49,7 @@ const std::string ProcessInfo::root() const { return m_root; }
 const std::string ProcessInfo::exe() const { return m_exe; }
 const std::unordered_map<std::string, std::string> ProcessInfo::environ() const { return m_environ; }
 int ProcessInfo::cpuUsage() const { return m_cpu_usage; }
-const ProcessInfo::userName() const
+const std::string ProcessInfo::userName() const
 {
     struct passwd* pass;
     pass = getpwuid(m_uids[0]); // real uid
